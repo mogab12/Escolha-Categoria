ffc4a1071a3f7f02a3abccfdf48bbd4bb40b2d7d
Gabriel Caram <gabrielccaram@usp.br>
2025-02-11 21:01:53 -0300

    Prepara projeto para deploy no Render

--- avaliacao_tema/avaliacao_tema/settings.py
+++ avaliacao_tema/avaliacao_tema/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1x2ym996@orp!sj7e=6k_w-202q7ul6kz4rvy(!s*@k!h5_fm_'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'gabriel')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'false'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 
 # Application definition
